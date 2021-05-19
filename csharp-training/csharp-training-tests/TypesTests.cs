e744ce92cce3adbe0bce92faf7fd974117b65f3f
Maciek <Maciek@maciek.pl>
2021-05-19 20:00:26 +0200

    another part of learning

--- csharp-training/csharp-training-tests/TypesTests.cs
+++ csharp-training/csharp-training-tests/TypesTests.cs
@@ -2,6 +2,8 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
 using System.Text;
 
 namespace csharp_training_tests
@@ -26,6 +28,52 @@ namespace csharp_training_tests
             //then
             number.Should().Be(expectedValue); // float cannot represent all decimal fractions, so most of them are only aproximations e.g. 0.1
 
+        }
+
+        [Test]
+        public void NumberShortcuts_002()
+        {
+            //given
+            var largeInt = 1_000_000_000;
+            var floatType = 1.00f;
+            var doubleType = 1.00D;
+            var decimalType = 10m;
+            var exponential = 1.5E2;
+            var exponentialDecimal = 1.5E2M;
+
+            //when
+            //then
+            Assert.Pass();
+        }
+
+        [Test]
+        public void NumberConversions_003()
+        {
+            //given
+            int i = 42;
+            double di = i; //42.00
+            float f = 3;
+            Trace.WriteLine(i / 5);
+            Trace.WriteLine(di / 5);
+            Trace.WriteLine(i / 5.0);
+            var result = di / f;
+            //int errorDoubleConversionToInt = di; 
+            int explicityConversionDoubleToInt = (int)di;
+            //int error = i / 1.0;
+            //int overflow = (int)3_000_000_001f; - error   
+
+            int minValue = unchecked(int.MaxValue + 1);
+
+            BigInteger bigInteger = int.MaxValue;
+            bigInteger += 1;
+
+
+
+            //when
+            //then
+            minValue.Should().Be(int.MinValue);
+            bigInteger.Should().Be(2_147_483_648);
+            Assert.Pass();
         }
     }
 }
