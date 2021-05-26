8251a4d8b9315c7974b63362223bcb4c393a50ea
Maciek <Maciek@maciek.pl>
2021-05-26 21:18:11 +0200

    learning

---
+++ csharp-training/csharp-training-tests/OperatorTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace csharp_training_tests
+{
+    [TestFixture]
+    public class OperatorTests
+    {
+        [Test]
+        public void Operators_001()
+        {
+            //given
+            int start = 1;
+
+            //when
+            var preIncrementation = ++start + 1;
+            preIncrementation.Should().Be(3);
+
+            start = 1;
+
+            var postIncrementation = start++ + 1;
+            postIncrementation.Should().Be(2);
+
+            start = 1;
+            var bitWiseOperator = false & ++start == 2;
+            start.Should().Be(2);
+
+            start = 1;
+            var andOperator = false && ++start == 2;
+
+            start.Should().Be(1);
+
+            var bitWiseOrOperator = true | ++start == 2;
+            start.Should().Be(2);
+
+            start = 1;
+            var orOperator = true || ++start == 2;
+
+            start.Should().Be(1);
+            var x = 1;
+            var y = 2;
+
+            int? max = x > y ? x : y; //conditional/tenary operator
+
+            var nullCoalescingOperator = max ?? 0; // null coalescing operator
+
+            x += 1; //compound assignment
+
+            //then
+        }
+    }
+}
--- csharp-training/csharp-training-tests/TypesTests.cs
+++ csharp-training/csharp-training-tests/TypesTests.cs
@@ -80,7 +80,7 @@ namespace csharp_training_tests
         public void StringType_004()
         {
             //given
-            int interationNumber = 100_000;
+            int iterationNumber = 100_000;
             string stringType = string.Empty;
             var stopwatch = new Stopwatch();
 
@@ -89,7 +89,7 @@ namespace csharp_training_tests
             //when
             #region string vs string builder
             stopwatch.Start();
-            for (int i = 0; i < interationNumber; i++)
+            for (int i = 0; i < iterationNumber; i++)
             {
                 stringType += "test";
             }
@@ -97,7 +97,7 @@ namespace csharp_training_tests
             Trace.WriteLine($"string: {stopwatch.ElapsedMilliseconds}, {stringType}");
 
             stopwatch.Restart();
-            for (int i = 0; i < interationNumber; i++)
+            for (int i = 0; i < iterationNumber; i++)
             {
                 stringBuilder.Append("test");
             }
@@ -106,13 +106,79 @@ namespace csharp_training_tests
             Trace.WriteLine($"string builder: {stopwatch.ElapsedMilliseconds}, {stringBuilder}");
             #endregion
 
-            Trace.WriteLine($"{interationNumber:f1}");
+            Trace.WriteLine($"{iterationNumber:f1}");
 
             var message = FormattableString.Invariant($"test");
+
+            var verbatimStringLiterals = @" test 
+                multiline";
+
+            var verbatimAndInterpolation = $@"test
+                multiline 
+                {iterationNumber}";
+
+            var text = @"Hello ""World""";
+            Trace.WriteLine(text);
 
 
             //then
             Assert.Pass();
         }
+
+        [Test]
+        public void Tuples_005()
+        {
+            //given
+            (int X, int Y) point = (1, 2);
+
+            var point_2 = (X: 4, Y: 8);
+
+            (int X1, int Y2) point_3 = point_2;
+
+            (int, int) test = (1, 1);
+
+            (double, float) conversion = (2d, 3f);
+
+            test = ((int, int))conversion;
+            conversion = test;
+
+            Trace.WriteLine(test != conversion);
+
+            (string Name, int Age) person = ("Maciek", 27);
+            string name;
+            int age;
+
+            (name, age) = person; //deconstruction
+            Trace.WriteLine($"{name}, {age}");
+
+            //when
+            //then
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Dynamic_006()
+        {
+            //given
+            dynamic variable = "ok";
+            variable = 10;
+
+            //when
+            //then
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Object_007()
+        {
+            //given
+            object variable = "ok";
+            variable = 10;
+
+            //when
+            //then
+            Assert.Pass();
+        }
+
     }
 }
