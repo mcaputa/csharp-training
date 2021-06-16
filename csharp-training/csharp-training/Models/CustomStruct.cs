83c64b0955ac464d52b6673d27ff76d281c69076
Maciek <Maciek@maciek.pl>
2021-06-16 22:21:06 +0200

    value types and reference types

--- csharp-training/csharp-training/Models/CustomStruct.cs
+++ csharp-training/csharp-training/Models/CustomStruct.cs
@@ -6,6 +6,6 @@ namespace csharp_training.Models
 {
     public struct CustomStruct
     {
-        public Counter Counter { get; set; } //can hold but i think it is bad practise 
+        public Counter Counter { get; set; } //can hold reference type but i think it is bad practise 
     }
 }
